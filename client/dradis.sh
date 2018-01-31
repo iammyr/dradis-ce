5d8951b900a5e059e89f526f151bc8265a25c337
iammyr <myriam.leggieri@mleggieri-03.workdayinternal.com>
2018-01-31 12:10:06 +0000

    user for the client script
     #minor
    
    Change-Id: I3a31069ded711fffbf9d62d984322a01856c5165

--- client/dradis.sh
+++ client/dradis.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 URL=http://0.0.0.0:3000/api/commands
+USER=dread
 
 if [ "$#" -lt 1 ]; then
     echo "You must pass to this script at least 1 bash command"
 fi
 
 # log commands to the server as a string
-curl --verbose -X POST -H "Content-Type: application/json" -d {"line": "$*" } -u 'dread'  $URL
+curl --verbose -X POST -H "Content-Type: application/json" -d {"line": "$*" } -u $USER  $URL
 
 # execute commands locally
 $*
